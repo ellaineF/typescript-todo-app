b2d451c93326a6a0c40a3161c5db0c87b70a1804
Ellaine <ellaineellie19@gmail.com>
2022-08-10 05:26:02 +0200

    fixed app test and todo list test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import App from './App';
+import mockData from './mockData';
+import fetchMock from 'jest-fetch-mock';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+fetchMock.enableMocks();
+
+beforeEach(() => {
+  fetchMock.once(JSON.stringify(mockData));
+});
+
+describe('<App /> tests', () => {
+  it('renders <App />', async () => {
+    render(<App />);
+    await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
+  });
+
 });
