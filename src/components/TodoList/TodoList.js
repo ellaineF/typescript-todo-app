43a21ed3c336beead0c49be2b3ab2e7cc79889c2
Ellaine <ellaineellie19@gmail.com>
2022-08-10 23:13:53 +0200

    Setting up TodoList.js

---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import TodoItem from '../TodoItem/TodoItem';
+
+const TodoList = ({ todos }) => (
+  <div>
+    {todos.map((t, i) => (
+      //<TodoItem key={i} todo={t} />
+      <div key={i} data-testid={`${t.id}`}>{t.title}</div>
+    ))}
+  </div>
+);
+
+export default TodoList;
