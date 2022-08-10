2daea6f22e153c5818cfa5ac0ce3d32469b95c6f
Ellaine <ellaineellie19@gmail.com>
2022-08-10 23:21:48 +0200

    Refactoring TodoList component

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,11 +1,11 @@
 import React from 'react';
-//import TodoItem from '../TodoItem/TodoItem';
+import TodoItem from '../TodoItem/TodoItem';
 
 const TodoList = ({ todos }) => (
   <div>
     {todos.map((t, i) => (
-      //<TodoItem key={i} todo={t} />
-      <div key={i} data-testid={`${t.id}`}>{t.title}</div>
+      <TodoItem key={i} todo={t} />
+      //<div key={i} data-testid={`${t.id}`}>{t.title}</div>
     ))}
   </div>
 );
