c0efaf123113af61842ba7c8678ea813e97a21a3
Ellaine <ellaineellie19@gmail.com>
2022-08-10 23:12:49 +0200

    Add logic for testing App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 import mockData from './mockData';
 import fetchMock from 'jest-fetch-mock';
@@ -16,4 +17,21 @@ describe('<App /> tests', () => {
     await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
   });
 
+  // it('should add a todo item', async () => {
+  //   fetchMock.once(
+  //     JSON.stringify({
+  //       userId: 3,
+  //       id: Math.floor(Math.random() * 100) + 1,
+  //       title: 'Do math homework',
+  //       completed: false,
+  //     }))
+
+  //   render(<App />);
+  //   await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
+
+  //   userEvent.type(screen.getByRole('textbox'), 'Do math homework');
+  //   userEvent.click(screen.getByText(/Add new todo/i));
+  //   await waitForElementToBeRemoved(() => screen.getByText(/saving/i));
+  //   expect(screen.getByText(/Do math homework/i)).toBeInTheDocument();
+  //   });
 });
